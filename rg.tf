8e8f2d0d12484b513e623b5b8c4b356e72573106
Ehetashamul Haque <hehetashamul@gmail.com>
2026-07-01 08:52:55 +0530

    Add new resource group for development environment

--- rg.tf
+++ rg.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-prod"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-dev"
+  location = "Central India"
+}
